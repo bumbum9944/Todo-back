4269e85b1f36807de5c87b06770f924c95f4b7c9
bumbum94 <tong940526@gmail.com>
2019-11-19 13:48:13 +0900

    get put delete 완성

--- todos/urls.py
+++ todos/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # localhost:8000/api/v1/todos
     path('todos/', views.todo_create),
+    # localhost:8000/api/v1/todos/10/
+    path('todos/<int:id>/', views.todo_detail),
 ]
